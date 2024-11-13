ff88979fc689454e66e92881949deaad5dccc10b
apy122 <alvaro.perez532@comunidadunir.net>
2024-11-13 23:30:00 +0100

    Listo para ultimo vistazo

--- src/javabeans/Fiesta.java
+++ src/javabeans/Fiesta.java
@@ -1,53 +1,48 @@
 package javabeans;
+
 import java.util.Scanner;
+
 public class Fiesta {
-	//Atributos
+	// Atributos
 	private String tipoFiesta;
 	private String direcccion;
 	private int bocadillos;
 	private int bebidas;
 	private int invitados;
-	
-	//Constructor  con atributos
-	public Fiesta(String tipoFiesta, String direcccion, int bocadillos, int bebidas, int invitados, String fecha,
-			String hora) {
-		super();
+
+	// Constructor con atributos y vacío
+	public Fiesta(String tipoFiesta, String direcccion, int bocadillos, int bebidas, int invitados) {
 		this.tipoFiesta = tipoFiesta;
 		this.direcccion = direcccion;
 		this.bocadillos = bocadillos;
 		this.bebidas = bebidas;
 		this.invitados = invitados;
 	}
-	
-	
-	//Constructor sin atributos
+
 	public Fiesta() {
-		super();
-		this.tipoFiesta = "Desconocido";
+		this.tipoFiesta = "Desconocida";
 		this.direcccion = "Desconocida";
 		this.bocadillos = 0;
 		this.bebidas = 0;
 		this.invitados = 0;
-		
-	
 	}
-	
-	//getter and setter
+
+	// Métodos getter y setter
 	public String getTipoFiesta() {
 		return tipoFiesta;
 	}
-	void setTipoFiesta(String tipoFiesta) {
+
+	public void setTipoFiesta(String tipoFiesta) {
 		this.tipoFiesta = tipoFiesta;
 	}
-
 
 	public String getDirecccion() {
 		return direcccion;
 	}
+
 	public void setDirecccion(String direcccion) {
 		this.direcccion = direcccion;
 	}
-
 
 	public int getBocadillos() {
 		return bocadillos;
@@ -60,6 +55,7 @@ public class Fiesta {
 	public int getBebidas() {
 		return bebidas;
 	}
+
 	public void setBebidas(int bebidas) {
 		this.bebidas = bebidas;
 	}
@@ -67,63 +63,50 @@ public class Fiesta {
 	public int getInvitados() {
 		return invitados;
 	}
+
 	public void setInvitados(int invitados) {
 		this.invitados = invitados;
 	}
-	
-	//Generamos la modificacion de toString
+
+	// Método para sumar invitados
+	public void invitar(int invitados) {
+		this.invitados += invitados;
+		System.out.println("Cantidad de invitados actual: " + this.invitados);
+	}
+
+	// Método para mostrar el número actual de invitados
+	public void mostrarInvitados() {
+		System.out.println("Número actual de invitados: " + this.invitados);
+	}
+
+	// Método para restar invitados
+	public void retirarInvitacion(int invitados) {
+		this.invitados -= invitados;
+		if (this.invitados < 0) {
+			this.invitados = 0; // Aseguramos que no haya un número negativo de invitados
+		}
+		System.out.println("Cantidad de invitados actual: " + this.invitados);
+	}
+
+	// Método para agregar invitados a través de entrada de usuario
+	public void agregarInvitaciones() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Introduce número de invitaciones: ");
+		int nuevosInvitados = scanner.nextInt();
+		this.invitados += nuevosInvitados;
+		System.out.println("Cantidad de invitados actual: " + this.invitados);
+	}
+
+	// Sobrecarga de agregarInvitaciones para agregar una cantidad específica
+	public void agregarInvitaciones(int invitados) {
+		this.invitados += invitados;
+		System.out.println("Cantidad de invitados después de agregar: " + this.invitados);
+	}
+
+	// Modificación de toString para imprimir los detalles de la fiesta
 	@Override
 	public String toString() {
-		return "Fiesta [tipoFiesta=" + tipoFiesta + ", direcccion=" + direcccion + ", bocadillos=" + bocadillos
+		return "Fiesta [tipoFiesta=" + tipoFiesta + ", direccion=" + direcccion + ", bocadillos=" + bocadillos
 				+ ", bebidas=" + bebidas + ", invitados=" + invitados + "]";
 	}
-	
-	//metodo y sobrecarga de invitar
-	public void invitar (int invitados) {
-		this.invitados= invitados ++;
-		this.invitados= this.invitados+ invitados;
-	}
-	public void invitar () {
-		System.out.println(this.invitados);
-	}
-	
-	//metodo y sobrecarga de retirar invitacion
-	public void retirarInvitacion (int invitados) {
-		this.invitados= invitados --;
-		this.invitados= this.invitados-invitados;
-	}
-	public void retirarinvitacion () {
-		System.out.println(this.invitados);
-	}
-	
-	//metodo y sobrecarga anadir invitacion
-	public void invitar() {
-		this.invitados= this.invitados++;
-	}
-	public void invitar(int invitados) {
-		invitar();
-		this.invitados=this.invitados+invitados;
-		System.out.println("cantidad de invitados:"+ invitados);
-	}
-
-	public void retirarInvitacion() {
-		this.invitados= this.invitados --;
-	}
-	public void retirarInvitacion(int invitados) {
-		retirarInvitacion();
-		this.invitados= this.invitados- invitados;
-		System.out.println("cantidad de invitados:"+ invitados);
-	}
-
-	
-	public void agregarInvitaciones() {
-	Scanner invitaciones= new Scanner(System.in);
-	System.out.println("Introduce numero de invitaciones: ");
-	this.invitados= this.invitados+ invitados;
-	}
-	public void agregarInvitaciones(int invitados) {
-		agregarInvitaciones();
-		this.invitados= this.invitados+invitados;
-		System.out.println("cantidad de invitados:"+ invitados);
-		}
 }
--- src/javabeans/FiestaTest.java
+++ src/javabeans/FiestaTest.java
@@ -11,5 +11,18 @@ public class FiestaTest {
 		DateTimeFormatter Formatter= DateTimeFormatter.ofPattern("HH:mm dd MM yy");
 		String dateString= fechaActual.format(Formatter);
 		System.out.println("La fecha actual es:"+ dateString);
-	}
+		
+		Fiesta fies1= new Fiesta();
+		System.out.println(fies1);
+		
+		Fiesta fies2= new Fiesta(null, null, 0, 0, 0);
+		fies1=fies2;
+		
+		System.out.println(fies2);
+		fies1.invitar(30);
+		fies1.retirarInvitacion(20);
+		fies1.agregarInvitaciones(20);
+		System.out.println(fies1);
+		System.out.println(fies2);
+		}
 }
